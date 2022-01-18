0eeee5070e8e8e594738b098f9f901879da06d9a
alshohatee <alshohateeaseel@gmail.com>
2022-01-17 20:34:33 -0500

    add the Remove Row, column and reset functions

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css"
 import Table from "./components/Table"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props )
 
     //Set initial state
     this.state = {
@@ -16,28 +16,60 @@ class App extends React.Component {
     }
 
     //Bind keyword
-    this.addRow = this.addRow.bind(this)
-    this.addColumn = this.addColumn.bind(this)
+    this.addRow = this.addRow.bind( this )
+    this.addColumn = this.addColumn.bind( this )
+    this.removeRow = this.removeRow.bind( this )
+    this.removeColumn = this.removeColumn.bind( this )
+    this.removeReset = this.removeReset.bind( this )
   }
 
   // This function adds a new row of cells
-  addRow(){
+  addRow() {
     // When there is no cell in the beginning
-    if (this.state.row === 0 && this.state.column === 0) {
-      this.setState({row: 1, column: 1, color: ""})
+    if ( this.state.row === 0 && this.state.column === 0 ) {
+      this.setState( { row: 1, column: 1, color: "" } )
     } else {
-      this.setState({row: this.state.row + 1})
+      this.setState( { row: this.state.row + 1 } )
     }
-    console.log("row: " + this.state.row + " column: " + this.state.column)
+    console.log( "row: " + this.state.row + " column: " + this.state.column )
   }
-  
+
   addColumn() {
     // When there is no cell in the beginning
-    if (this.state.row === 0 && this.state.column === 0) {
-      this.setState({row: 1, column: 1, color: ""})
+    if ( this.state.row === 0 && this.state.column === 0 ) {
+      this.setState( { row: 1, column: 1, color: "" } )
     } else {
-      this.setState({column: this.state.column + 1})
+
+      this.setState( { column: this.state.column + 1 } )
     }
+  }
+
+  removeRow() {
+    // When there is no cell in the beginning or if you remove all rows
+    if ( this.state.row <= 0 ) {
+      this.setState( { row: 0, column: 0, color: "" } )
+    } else {
+      this.setState( { row: this.state.row - 1 } )
+      console.log( "Row- Taking 1" )
+    }
+    console.log( this.state )
+  }
+
+  removeColumn() {
+    // When there is no cell in the beginning or if you remove all columns
+    if ( this.state.column <= 0 ) {
+      this.setState( { row: 0, column: 0, color: "" } )
+    } else {
+      this.setState( { column: this.state.column - 1 } )
+      console.log( "column- Taking 1" )
+    }
+    console.log( this.state )
+  }
+  removeReset() {
+    // Everything is default state
+    this.setState( { row: 0, column: 0, color: "" } )
+
+    console.log( this.state )
   }
 
 
@@ -45,25 +77,25 @@ class App extends React.Component {
     return (
       <>
         <main>
-            <div className= "controlPanel">
-              <div className="edit-btn">
-                <button className="control-btn" onClick={() => this.addRow()}>Add Row</button>
-                <button className="control-btn" onClick={() => this.addColumn()}>Add Column</button>
-                <button className="control-btn">Remove Row</button>
-                <button className="control-btn">Remove Column</button>
-                <button className="control-btn">Reset</button>
-              </div>
-              <div className="color-btn">
-                <button className="control-btn">Fill Unedited</button>
-                <button className="control-btn">Fill All</button>
-                <button className="control-btn">Clear All</button>
-                <button className="control-btn">Color</button>
-              </div>
-              
-              <div className="table">
-                <Table row={this.state.row} column={this.state.column} color={this.state.color}/>
-              </div>
+          <div className="controlPanel">
+            <div className="edit-btn">
+              <button className="control-btn" onClick={() => this.addRow()}>Add Row</button>
+              <button className="control-btn" onClick={() => this.addColumn()}>Add Column</button>
+              <button className="control-btn" onClick={() => this.removeRow()}>Remove Row</button>
+              <button className="control-btn" onClick={() => this.removeColumn()}>Remove Column</button>
+              <button className="control-btn" onClick={() => this.removeReset()}>Reset</button>
             </div>
+            <div className="color-btn">
+              <button className="control-btn">Fill Unedited</button>
+              <button className="control-btn">Fill All</button>
+              <button className="control-btn">Clear All</button>
+              <button className="control-btn">Color</button>
+            </div>
+
+            <div className="table">
+              <Table row={this.state.row} column={this.state.column} color={this.state.color} />
+            </div>
+          </div>
         </main>
       </>
     )
