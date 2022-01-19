6a35b996488277f1f1af6d6c5b93fc5e1c30d3c7
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-18 21:42:28 -0500

    revert back to old commit

---
+++ build/precache-manifest.8cdd1807199db264fd6426098fe881f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f737a13688ee779e781533c3765f3bf",
+    "url": "/assignment_8_dom_challenges/index.html"
+  },
+  {
+    "revision": "4ab088a6000f225d21a0",
+    "url": "/assignment_8_dom_challenges/static/css/main.fb274cc0.chunk.css"
+  },
+  {
+    "revision": "366d79e59ce010887710",
+    "url": "/assignment_8_dom_challenges/static/js/2.ec677add.chunk.js"
+  },
+  {
+    "revision": "4ab088a6000f225d21a0",
+    "url": "/assignment_8_dom_challenges/static/js/main.fc0e11da.chunk.js"
+  },
+  {
+    "revision": "cd1de8e79179f3560548",
+    "url": "/assignment_8_dom_challenges/static/js/runtime~main.46466a30.js"
+  }
+]);
